53de6a8661acfb3366bf1a7b0d616c0f8552355f
Sabil667 <msabi@myuax.com>
2023-11-09 13:06:33 +0100

    suma

--- Ex9/Ex9.cpp
+++ Ex9/Ex9.cpp
@@ -8,4 +8,25 @@ public:
     const char* what() const noexcept override {
         return "Materia ya registrada.";
     }
+
+};
+class Estudiante {
+public:
+    std::string nombre;
+    std::vector<std::string> materias;
+
+    Estudiante(const std::string& nombre) : nombre(nombre) {}
+
+    // Función para registrar una materia
+    void registrarMateria(const std::string& materia) {
+        // Verificar si la materia ya está registrada
+        for (const std::string& materiaRegistrada : materias) {
+            if (materiaRegistrada == materia) {
+                // Lanzar la excepción personalizada
+                throw MateriaYaRegistradaException();
+            }
+        }
+        // Si no está registrada, agregar la materia
+        materias.push_back(materia);
+    }
 };

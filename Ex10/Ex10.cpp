e13166ff2ff734e5e465c2a14e8d50d8d087465c
Sabil667 <msabi@myuax.com>
2023-11-09 13:14:12 +0100

    suma

--- Ex10/Ex10.cpp
+++ Ex10/Ex10.cpp
@@ -8,6 +8,13 @@ public:
     std::string materia,
     int añosExperiencia;
 
+    //Constructor para inicializar las propiedades
+    Profesor(const std::string& nombre, int edad, const std::string& materia, int añosExperiencia)
+    :nombre(nombre), edad(edad), materia(materia), añosExperiencia(añosExperiencia) {}
+
+
+
+
 
 
 

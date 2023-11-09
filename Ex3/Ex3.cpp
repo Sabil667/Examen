b7876682bbf06487495b45ed6e6378264b717870
Sabil667 <msabi@myuax.com>
2023-11-09 12:30:50 +0100

    suma

--- Ex3/Ex3.cpp
+++ Ex3/Ex3.cpp
@@ -6,3 +6,18 @@ void intercambiar(int *puntero1, int *puntero2) {
     *puntero1 = *puntero2;
     *puntero2 = temp;
 }
+int main() {
+    int a = 7;
+    int b = 14;
+
+    std::cout << "Numeros antes de hacer el cambio:" << std::endl;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    // Llamamos a la función para intercambiar los valores de a y b
+    intercambiar(&a, &b);
+
+    std::cout << "Despues del intercambio:" << std::endl;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+//2
+    return 0;
+}

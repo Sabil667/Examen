057f33d86f0a6c25646c4bda200bf7a45806323e
Sabil667 <msabi@myuax.com>
2023-11-09 12:50:58 +0100

    suma

--- Ex7/Ex7.cpp
+++ Ex7/Ex7.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 class RegistroAsistencia {
-    public
+public:
     //Estas son las propiedades de la clase
     std::string fecha;
     std::string estado;
@@ -11,5 +11,31 @@ class RegistroAsistencia {
     RegistroAsistencia(const std::string& fecha, const std::string& estado)
     : fecha(fecha), estado(estado) {}
 
+    //Funcion miembro para mostrar la informacion del estudiante
+    void mostrar_asistencia() {
+        std::cout << "Fecha de la clase: " << fecha << std::endl;
+        std::cout << "Estado de asistencia: " << estado << std::endl;
+    }
 
 };
+
+int main(){
+
+    std::string fecha;
+    std::string estado;
+
+    //Solicitar al usuario que introduzca los datos del estudiante
+    std::cout << "Introduce la fecha de la clase: ";
+    getline(std::cin, fecha);
+    std::cout<< "Introduce el estado de asistencia: ";
+    getline(std::cin, estado);
+
+    //Crear un objeto de la clase RegistroAsistencia
+    RegistroAsistencia registro_asistencia(fecha, estado);
+
+    //Mostrar la informacion del estudiante
+      registro_asistencia.mostrar_asistencia();
+
+
+
+}

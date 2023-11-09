2a3f313b8f0f7a84de7207a3673b73f1643aa0f3
Sabil667 <msabi@myuax.com>
2023-11-09 12:34:23 +0100

    suma

---
+++ Ex4/Ex4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+    //Hacemos una operacion que pueda tener una excepcion, en este caso la division entre 0.
+
+    int dividendo;
+    std::cout<<"Escoja un valor para el dividendo";
+
+
+}

dd026e690ad6e669fb27132e512d61f3e603960f
Sabil667 <msabi@myuax.com>
2023-11-09 12:37:52 +0100

    suma

--- Ex4/Ex4.cpp
+++ Ex4/Ex4.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 int main(){
     //Hacemos una operacion que pueda tener una excepcion, en este caso la division entre 0.
+ try{
+     int dividendo;
+     std::cout<<"Escoja un valor para el dividendo:";
+     std::cin>>dividendo;
+     int divisor;
+     std::cout<<"Escoja un valor para el divisor:";
+     std::cin>>divisor;
 
-    int dividendo;
-    std::cout<<"Escoja un valor para el dividendo";
-    std::cin>>dividendo;
-    int divisor;
-    std::cout<<"Escoja un valor para el divisor";
-    std::cin>>divisor;
+
+     //En este ejercicio se podra escoger el valor del dividendo y del divisor, si el divisor es 0, se lanza una excepción.
+
+     if (divisor == 0) {
+         throw std::runtime_error("Division por cero no permitida");
+     }
+
+     int resultado=dividendo/divisor;
+     std::cout << "El resultado de la division es: " << resultado << std::endl;
 
 
-
+ }
+ catch (const std::exception& e) {
+     // Capturamos y manejamos la excepción
+     std::cerr << "Error: " << e.what() << std::endl;
+ }
 
 }

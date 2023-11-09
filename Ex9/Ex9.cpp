62b6e0cd2dd25f3a5375c619af0494d90c1557c5
Sabil667 <msabi@myuax.com>
2023-11-09 13:06:09 +0100

    suma

--- Ex9/Ex9.cpp
+++ Ex9/Ex9.cpp
@@ -2,3 +2,10 @@
 #include <string>
 #include <vector>
 
+//Definicion de una excepcion en este caso personalizada "MateriaYaRegistrada"
+class MateriaYaRegistradaException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Materia ya registrada.";
+    }
+};

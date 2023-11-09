5bd5de99177f9b06514c2eccaa2d91c39e612bdd
Sabil667 <msabi@myuax.com>
2023-11-09 12:52:54 +0100

    suma

--- Ex8.cpp
+++ Ex8.cpp
@@ -11,3 +11,13 @@ public:
     Estudiante(const std::string& nombre, int edad, const std::string& grado)
             : nombre(nombre), edad(edad), grado(grado) {}
 };
+//Funcion para filtrar a los estudiantes por grado
+std::vector<Estudiante> filtrarEstudiantes(const std::vector<Estudiante>& estudiantes, const std::string& grado) {
+    std::vector<Estudiante> estudiantes_filtrados;
+    for (const Estudiante& estudiante : estudiantes) {
+        if (estudiante.grado == grado) {
+            estudiantes_filtrados.push_back(estudiante);
+        }
+    }
+    return estudiantes_filtrados;
+}

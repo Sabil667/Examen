e101426f79450abba27200888a667044f1dbee60
Sabil667 <msabi@myuax.com>
2023-11-09 12:17:14 +0100

    suma

---
+++ Ex1/suma.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    int a, b;
+    std::cout<<"Escoja un valor para a";
+    std::cin>>a;
+    std::cout<<"Escoja un valor para b";
+    std::cin>>b;
+    std::cout<<"La suma de a y b es: "<<a+b<<std::endl;
+    return 0;
+
+
+}
